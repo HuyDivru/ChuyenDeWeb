fce3ee785e8aff6d89190633d1c5642cf8bd17e3
TravisHuy <honhathuy098@gmail.com>
2024-05-20 11:27:38 +0700

    (admin) thêm chức năng thêm , xóa , sửa sản phẩm

---
+++ SpringBoot_Shop_Ban_Quan_Ao_BackEnd/src/main/java/com/shopbanquanao/admin/controller/AdminProductController.java
@@ -0,0 +1,50 @@
+package com.shopbanquanao.admin.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shopbanquanao.admin.repository.AdminProductRepository;
+import com.shopbanquanao.admin.service.AdminProductService;
+import com.shopbanquanao.model.Products;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+public class AdminProductController {
+	
+	
+	@Autowired
+	AdminProductService productService;
+	
+	@PostMapping("/saveProduct")
+	public ResponseEntity<?> saveEntity(@RequestBody Products product) {
+		return new ResponseEntity<>(productService.saveProduct(product),HttpStatus.CREATED);
+	}
+	@GetMapping("/listProduct")
+	public ResponseEntity<?> getAllProduct() {
+		return new ResponseEntity<>(productService.getAllProduct(), HttpStatus.OK);
+	}
+
+	@GetMapping("/listProduct/{id}")
+	public ResponseEntity<?> getProductById(@PathVariable Integer id) {
+		return new ResponseEntity<>(productService.getProductById(id), HttpStatus.OK);
+	}
+
+	@DeleteMapping("/deleteProduct/{id}")
+	public ResponseEntity<?> deleteProduct(@PathVariable Integer id) {
+		return new ResponseEntity<>(productService.deleteProduct(id), HttpStatus.OK);
+	}
+
+	@PostMapping("/editProduct/{id}")
+	public ResponseEntity<?> editProduct(@RequestBody Products product, @PathVariable Integer id) {
+		return new ResponseEntity<>(productService.editProduct(product, id), HttpStatus.CREATED);
+	}
+	
+}
---
+++ SpringBoot_Shop_Ban_Quan_Ao_BackEnd/src/main/java/com/shopbanquanao/admin/service/AdminProductServiceImpl.java
@@ -0,0 +1,55 @@
+package com.shopbanquanao.admin.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shopbanquanao.admin.repository.AdminProductRepository;
+import com.shopbanquanao.model.Products;
+
+@Service
+public class AdminProductServiceImpl implements AdminProductService{
+
+	@Autowired
+	AdminProductRepository productRepo;
+	
+	@Override
+	public Products saveProduct(Products product) {
+		return productRepo.save(product);
+	}
+
+	@Override
+	public List<Products> getAllProduct() {
+		return productRepo.findAll();
+	}
+
+	@Override
+	public Products getProductById(Integer id) {
+		return productRepo.findById(id).get();
+	}
+
+	@Override
+	public String deleteProduct(Integer id) {
+		Products product=productRepo.findById(id).get();
+		if(product!=null) {
+			productRepo.delete(product);
+			return "Product Delete Successfully";
+		}
+		return "Something wrong on server";
+	}
+
+	@Override
+	public Products editProduct(Products product, Integer id) {
+		Products oldProduct=productRepo.findById(id).get();
+		
+		oldProduct.setName(product.getName());
+		oldProduct.setPrice(product.getPrice());
+		oldProduct.setAdded_on(product.getAdded_on());
+		oldProduct.setCategory_id(product.getCategory_id());
+		
+		
+		return productRepo.save(oldProduct);
+	}
+	
+}
